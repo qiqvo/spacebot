30d9f9065d4dd5b883b035bfad4d0fcb586871ad
qiqvo <xanderprix@gmail.com>
2017-08-19 20:03:31 +0300

    new module structure

--- spacebot.py
+++ spacebot.py
@@ -11,44 +11,35 @@ https://github.com/elamperti/spacebot/blob/master/spacebot.py
 import os
 import telegram
 from telegram.ext import Updater, CommandHandler, Job
+
 import logging
+
 import arrow
 import datetime
-import requests
-from emoji import emojize
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from bot_cache import *
+from bot_interface import *
 
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-					level=logging.INFO)
-
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 user_timezone = ''
-str_user_alarm_minutes_before = 'user_alarm_minutes_before'
-ids_in_work_file = 'IDs.lst'
-subscribers = 'subscribers.lst'
+
+# in chat_data dict
+_user_alarm_minutes_before = 'user_alarm_minutes_before'
+_subscriber_5_min_before_the_launch = 's_5mb'
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-	update.message.reply_text("Hi! Nice to have you on board."
-					"We are to take off the planet at... It's hard to tell not knowing your timezone.")
-	user = update.message.from_user
-	logger.info('new user %s' % user.first_name)
-	help(bot, update)
-
-def help(bot, update):
-	help_message = 'usage: \n' \
-			'/set_timezone -- to set the timezone\n' \
-			'/next         -- to get the following launch\n' \
-			'/next 4       -- to get the following 4 launches\n' \
-			'/set_alarm 5  -- to set alarm on 5 minutes before the launch\n'
-	update.message.reply_text('')
+subscribers_filename = 'subscribers_5mb.lst' # list of "5 minutes before the launch"-subscribers
+subscribers = []
 
 def timezone(bot, update, args):
 	try:
+		update.message.reply_text()		
 		if not args[0]:
 			raise Exception('no input in timezone')
 		else:
@@ -62,7 +53,6 @@ def timezone(bot, update, args):
 		user_timezone = '+00:00'
 		update.message.reply_text('Something went wrong. Try to enter it once more')
 
-
 # context = [chat_id, event]
 def SendNotif(bot, job):
 	"""Function to send the alarm message"""
@@ -72,116 +62,98 @@ def SendNotif(bot, chat_id, event):
 	msg = generate_msg(event)
 	bot.send_message(chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
-def create_link(mode, next):
-	source = 'https://launchlibrary.net/1.2/launch?'
-	return source + 'mode=' + mode + '&' + 'next=' + next
-
-# todo add "many"
-def get_next_events(many): # many
-	logging.info("Fetching launches!")
-	
-	r = requests.get(create_link('verbose', str(many))) # add many 
-	events_lst = []
-
-	#debug
-	IDs_in_work.clear()
-
-	if r.status_code == 200:
-		for launch in r.json()['launches']:
-			if launch['id'] in IDs_in_work:
-				pass
-			else: 
-				IDs_in_work.append(launch['id'])
-			
-			event = {
-				'id' : launch['id']
-				"when": arrow.get(launch['isonet'], "YYYYMMDDTHHmmss?"),
-				"name": launch['name'],
-				"probability": launch['probability'], # -1 for unknown
-				"urls": launch['vidURLs'],
-				#"pic": launch['rocket']['imageURL'],
-				#"pic_sizes": launch['rocket']['imageSizes'],
-				"missions_count": len(launch['missions']),
-				"description": launch['missions'][0]['description'],
-				#"rocket": launch['rocket']['name']
-				'location': launch['location']['pads'][0]['name'] 
-			}
-			events_lst.append(event)
-
-	return events_lst
-
-
-def generate_msg(props):
-	message =  emojize(":rocket:", use_aliases=True)
-	message += ' **' + props['name'] + '**' + '\n'
-	message += 'A launch will happen ' + props['when'].humanize() + '!'
-	message += ' at ' + props['when'].format('HH:mm') + '\n'
-	message += 'Taking from **' + props['location'] + '**.\n'
-	message += '**Mission description**\n' + props['description'] + '.\n\n'
-
-#	if len(props['description']) > 0:
-#		message += props['description'] + '\n'
-	
-	if len(props['urls']) > 0:
-		message += 'Watch it here: \n'
-		for url in props['urls']:
-			message += '  • ' + url + '\n'
-	else:
-		message += 'Unfortunately there are no reported webcasts ' \
-				   + emojize(':disappointed_relieved:', use_aliases=True)
-	
-	return message
-
 # chat data is needed to implement user settings
 def SendNext(bot, update, chat_data):
 	SendNext(bot, update, [1], chat_data)
 
 def SendNext(bot, update, args, chat_data):
-	many = args[0]
-	events = get_next_events(many)
+	count = args[0]
+	events = get_next_events(count)
 	for event in events:
 		SendNotif(bot, update.message.chat_id, event)
 	
-def ScheduleNotification(events, when):
-	for event in events:
-		job = job_queue.run_once(SendNotif,
-			when=when, 
-			context=[chat_id, event])
-
-
-def update(bot, update, job_queue, chat_data):
-	"""Adds a job to the queue"""
-	chat_id = update.message.chat_id
-	try:
-		events = get_next_events()
-		for event in events:
-			try:
-				before_minutes = chat_data[str_user_alarm_minutes_before]
-				job_before_minutes = job_queue.run_once(SendNotif,
-					when=props['when'].shift(minutes=-before_minutes).datetime.replace(tzinfo=None), 
-					context=[chat_id, event])
-				chat_data['job_before_minutes'] = job_before_minutes
-			except KeyError as key_e:
-				pass
-	except Exception as e:
-		logger.error(str(e))
-		update.message.reply_text('Something went wrong!')
-
-
 def error(bot, update, error):
 	logger.warning('Update "%s" caused error "%s"' % (update, error))
 
-def set_alarm(bot, update, args, chat_data):
-	try:
-		before = int(args[0])
-		chat_data[str_user_alarm_minutes_before] = before
-	except ValueError as val_e:
-		update.message.reply_text('Enter integer!')
+# Modes are list, summary, verbose
+def create_link(mode, next):
+	source = 'https://launchlibrary.net/1.2/launch?'
+	return source + 'mode=' + mode + '&' + 'next=' + next
+
+def get_next_events(count):
+	events = get_cached_events(count) if count < k_cache_size else get_cached_events(k_cache_size)
+	if count > k_cache_size:
+		launches = pick_info(start=k_cache_size, end=(count+1))
+		for launch in launches:
+			events.append(create_event(launch))
+		
+	return events
+
+def create_event(launch):
+	event = {
+		'id' : launch['id'],
+		"when": arrow.get(launch['isonet'], "YYYYMMDDTHHmmss?"),
+		"name": launch['name'],
+		"probability": launch['probability'], # -1 for unknown
+		"urls": launch['vidURLs'],
+		#"pic": launch['rocket']['imageURL'],
+		#"pic_sizes": launch['rocket']['imageSizes'],
+		"missions_count": len(launch['missions']),
+		"description": launch['missions'][0]['description'],
+		#"rocket": launch['rocket']['name']
+		'location': launch['location']['pads'][0]['name'] 
+	}
+	return event
+
+# returns raw launches!! Its not the same as events
+def pick_info(start=0, end=k_cache_size):
+	logging.info("Picking...")
+	r = requests.get(create_link(mode='verbose', next=str(to)))
+
+	if r.status_code == 200:
+		return (r.json()['launches'])[start : end]
+	else:
+		logger.error('Error: bad request, while picking')
+		return []
 
 
+def update():
+	logging.info("Updating launches")
+	
+	launches = pick_info()
+
+	# clean cached ids which are no longer present
+	_first = 0
+	while launches[0]['id'] != cached_ids[_first]:
+		_first += 1
+	if _first != 0:
+		cached_ids[_first:]
+
+	for launch in launches:
+		event = create_event(launch)
+		if int(event['id']) in cache_dct:
+			check_up_to_date(event)
+		else: 
+			cache(event)
+
+
+def start(bot, update):
+	update.message.reply_text(welcome_message)
+	user = update.message.from_user
+	logger.info('new user %s' % user.first_name)
+	help(bot, update)
+
+def help(bot, update):
+	update.message.reply_text(help_message)
+
+
+	
 def main():
-
+	# todo do not cache offline
 	# todo cache 10 next launches and update them every day
+	# todo issue: for cached files -- if date is changed -- no effect on the queue
+	# todo clean cached ids as soon as start occured
+	# todo show notif of the ongoing launch mission if one is happening while your first chat 
 	# todo probability coefs
 	# todo if no vid, send pic 
 	# todo user settings: if to send msgs without videos
@@ -195,8 +167,8 @@ def main():
 
 	token = ''
 	if os.path.isfile('_token.token'):
-            with open('_token.token', 'r') as tokenFile:
-                token = tokenFile.read().replace('\n', '')
+		with open('_token.token', 'r') as tokenFile:
+			token = tokenFile.read().replace('\n', '')
 
 	updater = Updater(token)
 
@@ -215,9 +187,19 @@ def main():
 	dp.add_handler(CommandHandler("set_alarm", set_alarm, pass_args=True, pass_chat_data=True))
 	dp.add_handler(CommandHandler('next', SendNext, pass_args=True, pass_chat_data=True))
 	dp.add_handler(CommandHandler('next', SendNext, pass_chat_data=True))
+	
+	
+	dp.add_handler()
+	#dp.add_handler(CommandHandler('subscribe', subscribe, pass_chat_data=True))
 
 	# log all errors
 	dp.add_error_handler(error)
+
+	# to archive updating without user, we should use schedule module
+	scheduler = BackgroundScheduler()
+	update()
+	scheduler.add_job(update, 'interval', hours=5)
+	scheduler.start()
 
 	# Start the Bot
 	updater.start_polling()
@@ -244,3 +226,18 @@ if __name__ == '__main__':
 # 
 # 	update.message.reply_text('Timer successfully unset!')
 
+# def update(bot, update, job_queue, chat_data):
+# 	"""Adds a job to the queue"""
+# 	chat_id = update.message.chat_id
+# 	try:
+# 		events = get_next_events()
+# 		for event in events:
+# 			if _user_alarm_minutes_before in chat_data:
+# 				before_minutes = chat_data[_user_alarm_minutes_before]
+# 				job_before_minutes = job_queue.run_once(SendNotif,
+# 					when=props['when'].shift(minutes=-before_minutes).datetime.replace(tzinfo=None), 
+# 					context=[chat_id, event])
+# 				chat_data['job_before_minutes'] = job_before_minutes
+# 	except Exception as e:
+# 		logger.error(str(e))
+# 		update.message.reply_text('Something went wrong!')
