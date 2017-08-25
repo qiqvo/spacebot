ea54bce74f38d9e2731a74a624801c7e838ad587
qiqvo <xanderprix@gmail.com>
2017-08-25 20:55:26 +0300

    deleted timezone func edited imports and subscribe func and sendnext sender initialize in main with set_bot

--- spacebot.py
+++ spacebot.py
@@ -10,70 +10,47 @@ https://github.com/elamperti/spacebot/blob/master/spacebot.py
 import os
 
 import telegram
-from telegram.ext import (CommandHandler, Job, Updater, MessageHandler, 
-						Filters, RegexHandler, ConversationHandler)
+from telegram.ext import (CommandHandler, ConversationHandler, Filters, Job,
+						  MessageHandler, RegexHandler, Updater)
 
-from bot_info import get_next_events, update
-from bot_interface import *
+from bot_base import base
+from bot_interface import interface
 from bot_logging import logger, scheduler
 from bot_usersettings import users
+from bot_sender import sender
 
-def timezone(bot, update, args, chat_data):
-	try:
-		update.message.reply_text()		
-		if not args[0]:
-			raise Exception('no input in timezone')
-		else:
-			a = arrow.now().replace(tzinfo=args[0])
-			user_timezone = a.tzinfo
-		
-		update.message.reply_text('Set to %s.' % user_timezone)
-
-	except Exception as e:
-		logger.error(str(e))
-		user_timezone = '+00:00'
-		update.message.reply_text('Something went wrong. Try to enter it once more')
 
 def subscribe(bot, update, chat_data):
-	users.add_user(update.message.chat_id, 
+	user_id = update.message.chat_id
+	logger.info('Subscribe user %s' % user_id)
+	update.message.reply_text(interface.subscribe_message)
+	users.add_user(user_id, 
 				pref={'send_uncertain_launches': True})
 
 def send_uncertain_launches(bot, update, args, chat_data):
-	if args[0] == 'yes':
-		chat_data['send_uncertain_launches'] = True
-
 	user_id = update.message.chat_id
 	users.change(modify=[user_id, ['send_uncertain_launches', True]])
 
-# job.context = [chat_id, event]
-def SendNotif(bot, chat_id, event):
-	msg = generate_msg(event)
-	bot.send_message(chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
+def SendNext(bot, update, args):
+	count = args[0]
+	user_id = update.message.chat_id
+	logger.info("Sending user %s next %d events" % user_id, count)
+	sender.SendNext(user_id, count)
 
-# chat data is needed to implement user settings
-def SendNext(bot, update, chat_data, args=[1]):
-	count = int(args[0])
-	events = get_next_events(count)
-	for event in events:
-		SendNotif(bot, update.message.chat_id, event)
-	
 def error(bot, update, error):
 	logger.warning('Update "%s" caused error "%s"' % (update, error))
 
 def start(bot, update):
-	update.message.reply_text(welcome_message)
+	logger('Starting with user %s' % update.message.chat_id)
+	update.message.reply_text(interface.welcome_message)
 	user = update.message.from_user
 	logger.info('new user %s' % user.first_name)
 	help(bot, update)
 
 def help(bot, update):
-	update.message.reply_text(help_message)
+	update.message.reply_text(interface.help_message)
 	
 def main():
-	# TODO create event class with event remover (scheduler) 
-	# TODO get timezone from 
-	# 				https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000
-	# TODO discuss line 94 ```run_date=event['when'].shift(minutes=1),```
 	# TODO show notif of the ongoing launch mission if one is happening while your chat 
 	# TODO probability coefs
 	# TODO if no vid, send pic 
@@ -81,8 +58,6 @@ def main():
 	# TODO user settings: if to send msgs without videos
 	# 					  if to send uncertain launches
 	# 					  if to send pictures in what resolution is preferable
-	# TODO make subscriber list and 
-	#      send them all the notification in 30 mins
 	# TODO make available more info about the launch 
 	#	   maybe send launch id and make a func '/more_info id 1234'
 	# TODO make a full python lib for the source site ;;launchlibrary;;
@@ -93,27 +68,16 @@ def main():
 			token = tokenFile.read().replace('\n', '')
 
 	updater = Updater(token)
-
 	# Get the dispatcher to register handlers
 	dp = updater.dispatcher
+	sender.set_bot(dp.bot)
 
 	# on different commands - answer in Telegram
 	dp.add_handler(CommandHandler("start", start))
 	dp.add_handler(CommandHandler("help", start))
-	dp.add_handler(CommandHandler("update", update,
-								pass_job_queue=True,
-								pass_chat_data=True))
-	#dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
-	
-	#dp.add_handler(CommandHandler("set_timezone", timezone, pass_args=True))
-	#dp.add_handler(MessageHandler(Filters.location, location))
-	
-	#dp.add_handler(CommandHandler("set_alarm", set_alarm, pass_args=True, pass_chat_data=True))
 	dp.add_handler(CommandHandler('next', SendNext, 
-								pass_args=True, 
-								pass_chat_data=True))
+								pass_args=True))
 	
-	#dp.add_handler()
 	dp.add_handler(CommandHandler('subscribe', subscribe, pass_chat_data=True))
 	dp.add_handler(CommandHandler('send_uncertain_launches', send_uncertain_launches, 
 								pass_chat_data=True, pass_args=True))
@@ -121,7 +85,7 @@ def main():
 	# log all errors
 	dp.add_error_handler(error)
 
-	scheduler.add_job(update, 'interval', hours=5)
+	scheduler.add_job(base.update, 'interval', hours=5)
 	
 	users.get_from_file()
 	scheduler.add_job(users._change, 'interval', minutes=15)
@@ -139,32 +103,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-# def unset(bot, update, chat_data):
-# 	"""Removes the job if the user changed their mind"""
-# 
-# 	if 'job' not in chat_data:
-# 		update.message.reply_text('You have no active timer')
-# 		return
-# 
-# 	job = chat_data['job']
-# 	job.schedule_removal()
-# 	del chat_data['job']
-# 
-# 	update.message.reply_text('Timer successfully unset!')
-
-# def update(bot, update, job_queue, chat_data):
-# 	"""Adds a job to the queue"""
-# 	chat_id = update.message.chat_id
-# 	try:
-# 		events = get_next_events()
-# 		for event in events:
-# 			if _user_alarm_minutes_before in chat_data:
-# 				before_minutes = chat_data[_user_alarm_minutes_before]
-# 				job_before_minutes = job_queue.run_once(SendNotif,
-# 					when=props['when'].shift(minutes=-before_minutes).datetime.replace(tzinfo=None), 
-# 					context=[chat_id, event])
-# 				chat_data['job_before_minutes'] = job_before_minutes
-# 	except Exception as e:
-# 		logger.error(str(e))
-# 		update.message.reply_text('Something went wrong!')
