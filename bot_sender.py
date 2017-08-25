c965fc7fdb1b451531fdd9b93a46052d20962f83
qiqvo <xanderprix@gmail.com>
2017-08-25 20:41:04 +0300

    module to simplify sending msgs

---
+++ bot_sender.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Basics are copied from:
+https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/timerbot.py
+and 
+https://github.com/elamperti/spacebot/blob/master/spacebot.py
+"""
+
+import telegram
+
+from bot_base import base
+from bot_cache import cache
+from bot_interface import interface
+from bot_logging import logger, scheduler
+from bot_usersettings import users, Preferences
+
+
+class Sender:
+"""
+firstly set up the bot!!
+"""
+	def set_bot(self, bot):
+		self.bot = bot
+
+	def SendAll(self, event_id):
+		for user_id, user_pref in users.users:
+			event = base.get_event(event_id)
+			msg = interface.generate_msg(event, user_pref.prefs)
+			if msg:
+				self.Send(user_id, msg)
+
+
+	# TODO check if telegram.ParseMode.MARKDOWN works correctly
+	def Send(self, user_id, msg):
+		self.bot.send_message(user_id, text=msg,
+				parse_mode=telegram.ParseMode.MARKDOWN)
+			
+	def SendNext(self, user_id, count):
+		events = base.get_next_events(count)
+		for event in events:
+			msg = interface.generate_msg(event, users.users[user_id].prefs)
+			self.Send(user_id, msg)
+
+sender = Sender()
