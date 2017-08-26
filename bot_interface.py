4b55ae5be30ab6b3d9342d67cce3c50f30875628
qiqvo <xanderprix@gmail.com>
2017-08-26 10:17:41 +0300

    help msg update

--- bot_interface.py
+++ bot_interface.py
@@ -39,10 +39,9 @@ class Interface:
 						'We are to take off the planet at... '  \
 						"It's hard to tell not knowing your timezone."
 	help_message =  'usage: \n' \
-				'/set_timezone +00:00 	-- to set the timezone UTC\n' \
+				'/help                  -- to get this msg\n' \
 				'/next         			-- to get the following launch\n' \
 				'/next 4       			-- to get the following 4 launches\n' \
-				'/set_alarm 5  			-- to set alarm on 5 minutes before the launch\n' \
 				'/send_uncertain_launches yes -- to send ...'
 
 	# TODO write subscibe answer msg 
