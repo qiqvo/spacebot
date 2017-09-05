544a88c2fbe5b07a0e1c32ffb9c01ea783dc9a2f
Mitya <therealmitya@icloud.com>
2017-09-05 15:38:46 +0300

    Revert "Test"
    This reverts commit c4d22f516f0262ecc7f29e38c93ba61bb258c498.

--- bot_interface.py
+++ bot_interface.py
@@ -69,8 +69,8 @@ class Interface:
 				''
 
 	# TODO write subscibe answer msg
-	subscribe_message = 'BLA-BLA-BLA' # you will be subscribed in 15 minutes at most! Thanks!
-	unsubscribe_message = 'BLA-BLA-BLA' # you will be unsubscribed in 15 minutes at most!
+	subscribe_message = 'bla-fghjkl-bla' # you will be subscribed in 15 minutes at most! Thanks!
+	unsubscribe_message = 'bla-bla-bla' # you will be unsubscribed in 15 minutes at most!
 
 
 
